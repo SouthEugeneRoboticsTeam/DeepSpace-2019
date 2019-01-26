f538988df374f95514b08f845d2f1f4e07f20781
Andrew Dassonville <dassonville.andrew@gmail.com>
2019-01-26 09:01:18 -0800

    Bump meanlib version

--- src/main/kotlin/org/sert2521/deepspace/drivetrain/Drivetrain.kt
+++ src/main/kotlin/org/sert2521/deepspace/drivetrain/Drivetrain.kt
@@ -14,7 +14,7 @@ import org.team2471.frc.lib.motion_profiling.following.ArcadeRobot
  * The robot's drive system.
  */
 object Drivetrain : Subsystem("Drivetrain", Drivetrain::teleopDrive), ArcadeRobot {
-    override val config = drivetrainConfig
+    override val parameters = drivetrainConfig
 
     val leftDrive = TalonSRX(Talons.DRIVE_LEFT_FRONT, Talons.DRIVE_LEFT_REAR).config {
         feedbackCoefficient = ticksToFeet(1)
--- src/main/kotlin/org/sert2521/deepspace/drivetrain/DrivetrainConstants.kt
+++ src/main/kotlin/org/sert2521/deepspace/drivetrain/DrivetrainConstants.kt
@@ -1,11 +1,17 @@
 package org.sert2521.deepspace.drivetrain
 
-import org.team2471.frc.lib.motion_profiling.following.ArcadeRobot
+import org.team2471.frc.lib.motion_profiling.following.ArcadeParameters
+import org.team2471.frc.lib.motion_profiling.following.RobotParameters
 
 internal const val DISTANCE_P = 0.075
 internal const val DISTANCE_D = 0.003
 
-internal val drivetrainConfig = ArcadeRobot.Config(
+internal val robotConfig = RobotParameters(
+    robotWidth = 28.0 / 12.0,
+    robotLength = 32.0 / 12.0
+)
+
+internal val drivetrainConfig = ArcadeParameters(
     trackWidth = 0.5,
     scrubFactor = 1.115,
     driveTurningP = 0.008,
